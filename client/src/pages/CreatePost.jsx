2497faff46bedd7231c46d8063df85b33eff7d48
Rois <roisfaozi55@gmail.com>
2023-02-08 13:01:47 +0700

    change api endpoint

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,13 +19,16 @@ export default function CreatePost() {
     if (form.prompt) {
       try {
         setGeneratingIMG(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          'https://apidalle.vercel.app/api/v1/dalle',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
 
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
@@ -45,13 +48,16 @@ export default function CreatePost() {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(form),
-        });
+        const response = await fetch(
+          'https://apidalle.vercel.app/api/v1/post',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form),
+          }
+        );
         await response.json();
         navigate('/');
       } catch (error) {
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://apidalle.vercel.app/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
